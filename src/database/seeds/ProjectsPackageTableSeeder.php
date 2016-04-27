02395510ddc6113e48b7e5820f13d10d382f40e5
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-04-27 18:00:57 +0200

    minor fix

--- src/database/seeds/ProjectsPackageTableSeeder.php
+++ src/database/seeds/ProjectsPackageTableSeeder.php
@@ -8,7 +8,7 @@ class ProjectsPackageTableSeeder extends Seeder
     public function run()
     {
         Package::insert([
-            ['id_012' => '6', 'name_012' => 'Projects Package', 'folder_012' => 'projects', 'sorting_012' => 6, 'active_012' => '0']
+            ['id_012' => '6', 'name_012' => 'Projects Package', 'folder_012' => 'projects', 'sorting_012' => 6, 'active_012' => false]
         ]);
     }
 }
